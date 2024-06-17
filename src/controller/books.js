7d56cfee9ab2dc754570754c3ed5568c9ae6b54e
yashu <mandavilliy@gmail.com>
2024-06-17 18:00:38 +0400

    added book routes

--- src/controller/books.js
+++ src/controller/books.js
@@ -3,12 +3,86 @@
 // const fs = require('fs')
 
 // uncomment the following line if you want to use mongodb
-// const Books = require("../models/books");
+const mongoose = require("mongoose");
+const createError = require('http-errors');
+const Books = require("../models/books");
 
 class Controller {
-  Controller() {}
+  constructor() {
+    // constructor logic if needed
+  }
 
-  // add your controllers here
+  async getBooks(req, res) {
+    try {
+      const results = await Books.find();
+      res.send(results);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send(error.message); // Properly handle the error response
+    }
+  }
+
+  async findBookById(req, res, next) {
+    const id = req.params.id;
+    try {
+      const result = await Books.findById(id);
+      if (!result) {
+        throw createError(404, 'Book does not exist.');
+      }
+      res.send(result);
+    } catch (error) {
+      console.log(error.message);
+      if (error instanceof mongoose.CastError) {
+        next(createError(400, 'Invalid Book id'));
+        return;
+      }
+      next(error);
+    }
+  }
+
+  async createNewBook(req, res, next) {
+    try {
+      const book = new Books({
+        title: req.body.name,
+        page: req.body.page,
+        authorID: new mongoose.Types.ObjectId(),
+        price: req.body.price,
+        isDeleted: false,
+        isIssued: false
+      });
+      const result = await book.save();
+      res.send(result);
+    } catch (error) {
+      console.log(error.message);
+      if (error.name === 'ValidationError') {
+        next(createError(422, error.message));
+        return;
+      }
+      next(error);
+    }
+  }
+
+  async updateABook(req, res, next){
+    try {
+      const id = req.params.id;
+      const updates = req.body;
+      const options = { new: true };
+
+      const result = await Books.findByIdAndUpdate(id, updates, options);
+      if (!result) {
+        throw createError(404, 'Book does not exist');
+      }
+      res.send(result);
+    } catch (error) {
+      console.log(error.message);
+      if (error instanceof mongoose.CastError) {
+        return next(createError(400, 'Invalid Book Id'));
+      }
+
+      next(error);
+    }
+  }
+  // add your other controllers here
 }
 
 module.exports = Controller;
--- src/routes/book.js
+++ src/routes/book.js
@@ -4,6 +4,9 @@ const Controller = require("../controller/books");
 const controller = new Controller();
 
 // add your RESTful api routes here
-// ...
+routes.get("/books",controller.getBooks);
+routes.get('/books/:id', controller.findBookById);
+routes.post('/books', controller.createNewBook);
+routes.patch('/books/:id', controller.updateABook);
 
 module.exports = routes;
