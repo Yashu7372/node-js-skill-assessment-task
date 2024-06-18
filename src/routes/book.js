949051c7bcb8fd9526a95114c90dc1f33a5a14a5
yashu <mandavilliy@gmail.com>
2024-06-18 12:08:03 +0400

    added views

--- src/routes/book.js
+++ src/routes/book.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const routes = express.Router();
 const Controller = require("../controller/books");
+const services = require('../services/render');
 const controller = new Controller();
 
 // add your RESTful api routes here
-routes.get("/books",controller.getBooks);
-routes.get('/books/:id', controller.findBookById);
-routes.post('/books', controller.createNewBook);
-routes.patch('/books/:id', controller.updateABook);
+routes.get('/', services.homeRoutes);
+routes.get('/add-book', services.add_book)
+routes.get("/api/books",controller.getBooks);
+routes.get('/api/books/:id', controller.findBookById);
+routes.post('/api/books', controller.createNewBook);
+routes.patch('/api/books/:id', controller.updateABook);
 
 module.exports = routes;
