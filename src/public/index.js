72b709ef4287ecb7877007ae41d34b8c67fffc61
yashu <mandavilliy@gmail.com>
2024-06-18 12:59:45 +0400

    added env and api links

--- src/public/index.js
+++ src/public/index.js
@@ -1,66 +1,67 @@
-$("#add_book").submit(function(event){
-    alert("Data Inserted Successfully!");
-    window.location.href = "/";
-})
 
-function handleUpdate(bookId) {
-    
-    // Make a GET request to fetch the book details
-    $.ajax({
-        url: `http://localhost:5000/api/books/${bookId}`,
-        method: 'GET',
-        success: function(response) {
-            const isIssued = response.isIssued;
-            const updatedValue = !isIssued
-            
-            // Make a PATCH request to update the book's issue status
-            $.ajax({
-                url: `http://localhost:5000/api/books/${bookId}`,
-                method: 'PATCH',
-                data: { isIssued: updatedValue },
-                success: function() {
-                    alert("Data Updated Successfully!");
-                    location.reload();
-                },
-                error: function(error) {
-                    console.error(error);
-                }
-            });
-        },
-        error: function(error) {
-            console.error(error);
-        }
+const baseUrl = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
+    console.log(baseUrl);
+
+    $("#add_book").submit(function(event){
+        alert("Data Inserted Successfully!");
+        window.location.href = "/";
     });
-}
 
-function handleDelete(bookId) {
-    
-    // Make a GET request to fetch the book details
-    $.ajax({
-        url: `http://localhost:5000/api/books/${bookId}`,
-        method: 'GET',
-        success: function(response) {
-            const isDeleted = response.isDeleted;
-            const updatedValue = !isDeleted
-            
-            // Make a PATCH request to update the book's issue status
-            $.ajax({
-                url: `http://localhost:5000/api/books/${bookId}`,
-                method: 'PATCH',
-                data: { isDeleted: updatedValue },
-                success: function() {
-                   updatedValue? alert("Data Deleted Successfully!"):alert("Data Restored Successfully!");
-                    location.reload();
-                },
-                error: function(error) {
-                    console.error(error);
-                }
-            });
-        },
-        error: function(error) {
-            console.error(error);
-        }
-    });
-}
+    function handleUpdate(bookId) {
+        // Make a GET request to fetch the book details
+        $.ajax({
+            url: `${baseUrl}/api/books/${bookId}`,
+            method: 'GET',
+            success: function(response) {
+                const isIssued = response.isIssued;
+                const updatedValue = !isIssued;
+                
+                // Make a PATCH request to update the book's issue status
+                $.ajax({
+                    url: `${baseUrl}/api/books/${bookId}`,
+                    method: 'PATCH',
+                    data: { isIssued: updatedValue },
+                    success: function() {
+                        alert("Data Updated Successfully!");
+                        location.reload();
+                    },
+                    error: function(error) {
+                        console.error(error);
+                    }
+                });
+            },
+            error: function(error) {
+                console.error(error);
+            }
+        });
+    }
 
+    function handleDelete(bookId) {
+        // Make a GET request to fetch the book details
+        $.ajax({
+            url: `${baseUrl}/api/books/${bookId}`,
+            method: 'GET',
+            success: function(response) {
+                const isDeleted = response.isDeleted;
+                const updatedValue = !isDeleted;
+                
+                // Make a PATCH request to update the book's delete status
+                $.ajax({
+                    url: `${baseUrl}/api/books/${bookId}`,
+                    method: 'PATCH',
+                    data: { isDeleted: updatedValue },
+                    success: function() {
+                        updatedValue ? alert("Data Deleted Successfully!") : alert("Data Restored Successfully!");
+                        location.reload();
+                    },
+                    error: function(error) {
+                        console.error(error);
+                    }
+                });
+            },
+            error: function(error) {
+                console.error(error);
+            }
+        });
+    }
 
